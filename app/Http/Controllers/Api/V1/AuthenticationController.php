9ae0165d8bd3829d4b944eacbb739d7f16195fcf
Venci <oolja@abv.bg>
2024-01-07 14:23:59 +0200

    Update composer and fix phpstan errors

--- app/Http/Controllers/Api/V1/AuthenticationController.php
+++ app/Http/Controllers/Api/V1/AuthenticationController.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class AuthenticationController extends Controller
 {
@@ -21,6 +22,10 @@ class AuthenticationController extends Controller
 
         $user = Auth::user();
 
+        if (is_null($user)) {
+            throw new NotFoundHttpException('User not found');
+        }
+
         $token = $user->createToken("$user->type-token", [$user->type]);
 
         return response()->json(['token' => $token->plainTextToken]);
@@ -28,7 +33,7 @@ class AuthenticationController extends Controller
 
     public function signOut(): JsonResponse
     {
-        Auth::user()->tokens()->delete();
+        Auth::user()?->tokens()->delete();
         return response()->json([
             'status' => 'ok',
             'message' => 'Successfully logged out',
